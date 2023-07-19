04a1344e258e56b42b60e86918685c4d8ccb68e6
Sergio <sergio.contardi@telefonica.com>
2023-07-19 15:18:51 -0300

    Google SingIng

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,9 @@
-const { response } = require("express");
+const { response, request } = require("express");
 const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 
 
@@ -58,8 +59,63 @@ const login = async( req, res = response ) =>{
 
 }
 
+
+const googleSingIn = async( req = request, res = response ) =>{
+
+    const { id_token } = req.body;
+
+    try {
+
+        const {nombre, img, correo} = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({correo});
+
+        if(!usuario){
+
+            // Si no existe el usuario, lo creamos con esta DATA
+
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            }
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+
+        // SI el usuario esta en false en la base denegamos la entrada
+
+        if(!usuario.estado){
+           return res.status(401).json({
+            msg: 'Pongase en contacto con el admin. Usuario bloqueado' 
+        })
+        }
+
+        // Generar el JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        })
+        
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'NO se pudo verificar token'
+        })
+    }
+
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSingIn
+
 }
 
 
