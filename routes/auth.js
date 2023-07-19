386e6f2e6bab54e4c6055a233097dfb7392317ad
Sergio <sergio.contardi@telefonica.com>
2023-07-19 15:21:02 -0300

    Google Sing ing Sing Out

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 
 const { check } = require('express-validator');
-const { login } = require('../controllers/auth');
+const { login, googleSingIn } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 
@@ -16,6 +16,11 @@ router.post('/login', [
     check('password', ' La contraseña es obligatoria').not().isEmpty(),
     validarCampos
 ] ,login );
+
+router.post('/google', [
+    check('id_token', 'id_token de google es necesario').not().isEmpty(),
+    validarCampos
+] ,googleSingIn );
 
 
 
